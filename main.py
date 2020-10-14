89450ac0dd1085d54295c6326e4f97ed68de2390
Evsey <evseyru1@mail.ru>
2020-10-15 03:39:20 +0500

    added recognition of whether eyes are closed (somewhat works), removed redundant requirements, added eye cascade

--- main.py
+++ main.py
@@ -1,31 +1,157 @@
-import cv2
+# Identify pupils. Based on beta 1
+
 import numpy as np
-import imutils
+import cv2
+import time
 
-cascade_path = "haarcascade_frontalface_default.xml"
-face_cascade = cv2.CascadeClassifier(cascade_path)
+cap = cv2.VideoCapture(0)  # 640,480
+w = 640
+h = 480
 
-cam_num = -1
-cap = cv2.VideoCapture(cam_num)
+time_start = time.time()
 
+eye_timer = 5  # in seconds
+eye_delta_timer = 1  # in seconds
 
-def main():
-	if not (cap.isOpened()):
-		print("cant open camera")
-		return
-	while True:
+eyes_opened = True
+was_opened = True
+cur_opened = True
+last_cur_opened = True
 
-		ret, frame = cap.read()
-		image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-		for (x, y, w, h) in faces:
-			# Create rectangle aroun#d the face
-			cv2.rectangle(image, (x - 20, y - 20), (x + w + 20, y + h + 20), (255, 25, 25), 1)
-		cv2.imshow("img", image)
-		k = cv2.waitKey(30)
-		if k == 27:
-			break
+alarming = False
 
-if __name__ == "__main__":
-	main()
+last_alarmed = time_start
+last_state_changed = time_start
+last_change = time_start
+last_static = time_start
+
+while (cap.isOpened()):
+    eyes_opened = False
+    ret, frame = cap.read()
+    if ret == True:
+
+        # downsample
+        # frameD = cv2.pyrDown(cv2.pyrDown(frame))
+        # frameDBW = cv2.cvtColor(frameD,cv2.COLOR_RGB2GRAY)
+
+        # detect face
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        faces = cv2.CascadeClassifier('haarcascade_eye.xml')
+        detected = faces.detectMultiScale(frame, 1.3, 5)
+
+        # faces = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        # detected2 = faces.detectMultiScale(frameDBW, 1.3, 5)
+
+        pupilFrame = frame
+        pupilO = frame
+        windowClose = np.ones((5, 5), np.uint8)
+        windowOpen = np.ones((2, 2), np.uint8)
+        windowErode = np.ones((2, 2), np.uint8)
+
+        # draw square
+        for (x, y, w, h) in detected:
+            cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 0, 255), 1)
+            cv2.line(frame, (x, y), ((x + w, y + h)), (0, 0, 255), 1)
+            cv2.line(frame, (x + w, y), ((x, y + h)), (0, 0, 255), 1)
+            pupilFrame = cv2.equalizeHist(frame[int(y + (h * .25)):(y + h), x:(x + w)])
+            pupilO = pupilFrame
+            ret, pupilFrame = cv2.threshold(pupilFrame, 75, 255, cv2.THRESH_BINARY)  # 50 ..nothin 70 is better
+            pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_CLOSE, windowClose)
+            pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_ERODE, windowErode)
+            pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_OPEN, windowOpen)
+
+            # so above we do image processing to get the pupil..
+            # now we find the biggest blob and get the centriod
+
+            threshold = cv2.inRange(pupilFrame, 250, 255)  # get the blobs
+            contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+            # if there are 3 or more blobs, delete the biggest and delete the left most for the right eye
+            # if there are 2 blob, take the second largest
+            # if there are 1 or less blobs, do nothing
+
+            if len(contours) >= 2:
+
+                #print("pupil at", time.time() - time_start)
+                # find biggest blob
+                maxArea = 0
+                MAindex = 0  # to get the unwanted frame
+                distanceX = []  # delete the left most (for right eye)
+                currentIndex = 0
+                for cnt in contours:
+                    area = cv2.contourArea(cnt)
+                    center = cv2.moments(cnt)
+                    if center['m00'] != 0:
+                        cx = int(center["m10"] / center["m00"])
+                        cy = int(center["m01"] / center["m00"])
+                    else:
+                        cx, cy = 0, 0
+                    distanceX.append(cx)
+                    if area > maxArea:
+                        maxArea = area
+                        MAindex = currentIndex
+                    currentIndex = currentIndex + 1
+
+                del contours[MAindex]  # remove the picture frame contour
+                del distanceX[MAindex]
+
+            eye = 'right'
+
+            if len(contours) >= 2:  # delete the left most blob for right eye
+                if eye == 'right':
+                    edgeOfEye = distanceX.index(min(distanceX))
+                else:
+                    edgeOfEye = distanceX.index(max(distanceX))
+                del contours[edgeOfEye]
+                del distanceX[edgeOfEye]
+
+            if len(contours) >= 1:  # get largest blob
+                maxArea = 0
+                for cnt in contours:
+                    area = cv2.contourArea(cnt)
+                    if area > maxArea:
+                        maxArea = area
+                        largeBlob = cnt
+
+            if len(largeBlob) > 0:
+                eyes_opened = True
+                center = cv2.moments(largeBlob)
+                cx, cy = int(center['m10'] / center['m00']), int(center['m01'] / center['m00'])
+                cv2.circle(pupilO, (cx, cy), 5, 255, -1)
+
+        if eyes_opened != was_opened:
+            was_opened = eyes_opened
+            last_change = time.time()
+            if time.time() - last_static >= eye_delta_timer:
+                cur_opened = False
+        else:
+            if time.time() - last_change >= eye_delta_timer:
+                cur_opened = was_opened
+                last_static = time.time()
+
+        if cur_opened != last_cur_opened:
+            last_cur_opened = cur_opened
+            print("at", time.time() - time_start, "cur opened =", cur_opened)
+            last_state_changed = time.time()
+        else:
+            if not cur_opened and not alarming and time.time() - last_state_changed >= eye_timer:
+                alarming = True
+                print("alarm at", time.time() - time_start)
+
+        if alarming:
+            if cur_opened:
+                print("alarming stopped at", time.time() - time_start)
+                alarming = False
+
+        # show picture
+        cv2.imshow('frame', pupilO)
+        cv2.imshow('frame2', pupilFrame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# else:
+# break
+
+# Release everything if job is finished
+cap.release()
+cv2.destroyAllWindows()
